c95c1ba55108f2a88a767b151c38947fe4e5a70a
Edward Mason <mynameisprivate3@gmail.com>
2025-04-10 15:47:30 +0100

    Add Assessment data, script

---
+++ Assessment/script.R
@@ -0,0 +1,9 @@
+library(tidyverse)
+
+data <- read.csv("data.csv", header=T, sep=",")
+
+mod <- lm(rainfall~., data = data)
+mod_step <- step(mod, direction = "both")
+
+summary(mod_step)
+plot(mod_step)
